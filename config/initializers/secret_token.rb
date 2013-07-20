6659ebf936616058b748bf31d71cbb04f6626078
Steve <stevemargc@gmail.com>
2013-07-20 13:09:35 -0400

    initial commit plus added home controller and home view with routing and added rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Combat::Application.config.secret_token = '8f2dbc79d716ab357388f8001ab9a4c96ca2ff797e7fb9469b4a84c1c74f05507301d98b25c48562c3866b507b4bd76018a66db1bcbd6d0dac068529910a426e'
