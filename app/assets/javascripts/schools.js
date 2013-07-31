9dc5b0901bef113010fe08c65746cf40c965d362
Steve <stevemargc@gmail.com>
2013-07-31 17:20:33 -0400

    adding session to view to see why not behaving in heroku as it is in local

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -2,6 +2,7 @@ jQuery(function($) {
 
 $('input[type=checkbox]').click(function(){
   $.post('update_session', { school_id: $(this).val(), checked: $(this).prop('checked')  });
+alert($(this).val());
 });
 
 
