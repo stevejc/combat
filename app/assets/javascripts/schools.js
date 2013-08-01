7124c28d46fc0408df23b875dea2d32276c088c9
Steve <stevemargc@gmail.com>
2013-08-01 09:38:09 -0400

    removed the debugging items for heroku deployment of school compare redo.

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -2,9 +2,7 @@ jQuery(function($) {
 
 $('input[type=checkbox]').click(function(){
   $.post('update_session', { school_id: $(this).val(), checked: $(this).prop('checked')  });
-alert($(this).val());
 });
-
 
 $('.remove-school').tooltip();
 
